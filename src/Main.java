c670dc97f43b25ba0313e935b408db7d22451224
Elf <elvirius.gr@gmail.com>
2025-03-04 23:10:45 +0200

    commit3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String name;
-        int adc;
+        String name = "Sara";
+        int adc = 0;
 
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! %s");
     }
 }
